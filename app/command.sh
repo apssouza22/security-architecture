b06484473d29864cbe73dcf9988028517279a0ef
Alexsandro Souza <apssouza22@gmail.com>
2025-06-25 16:57:40 +0200

    use templates

--- app/command.sh
+++ app/command.sh
@@ -3,11 +3,12 @@
 iptables -t nat -A OUTPUT -p tcp --dport 80 -j DNAT --to-destination 127.0.0.1:81
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
-# Start the nginx process
-nginx -g 'daemon off;' &
+/docker-entrypoint.sh nginx
+echo "**** Started nginx ****"
 
 # Start the node process
 node /app/server.js &
+echo "**** Started app ****"
 
 # Wait for any process to exit
 wait -n
